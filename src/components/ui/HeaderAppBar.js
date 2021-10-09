dd7ddd372aa96d0f293593d0139680b7a6952ab2
Jhonatan Soto <jhonrocker2012@gmail.com>
2021-10-08 22:41:58 -0400

    create appBar

---
+++ src/components/ui/HeaderAppBar.js
@@ -0,0 +1,31 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import React from 'react';
+
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  toolBar: {
+    flexGrow: 1,
+  },
+  appbar: {
+    alignItems: 'center',
+  },
+});
+
+export const HeaderAppBar = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.toolBar}>
+      <AppBar position='static' className={classes.appbar}>
+        <Toolbar>
+          <Typography variant='h6' component='div' align='left' sx={{ p: 1 }}>
+            CV-CREATOR
+          </Typography>
+          <DocumentScannerIcon />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
